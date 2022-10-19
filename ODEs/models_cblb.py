7ed7721ec1bd3cbde5588ad5a7721bced5efe52a
Miha Moškon <miha.moskon@fri.uni-lj.si>
2022-10-19 14:36:49 +0200

    model building

---
+++ ODEs/models_cblb.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+# a model of inverter
+def not_cell(state, params):
+    L_X, x, y, N_X, N_Y = state
+    delta_L, gamma_L_X, n_y, theta_L_X, eta_x, omega_x, m_x, delta_x, rho_x = params
+
+    # presume that the molecules are degraded in the same strain as they are produced
+    N_Y = N_X
+
+
+    f = gamma_L_X * (y ** n_y)/(1 + (theta_L_X*y)**n_y )
+    dL_X_dt = N_X * (f - delta_L * L_X)
+
+    dx_dt = N_X * (eta_x * (1/(1+ (omega_x*L_X)**m_x))) - N_Y * (delta_x * x) - rho_x * x
+
+    return dL_X_dt, dx_dt
+
+# a model of driver
+def yes_cell(state, params):
+    x, y, N_X, N_Y = state
+    gamma_x, n_y, theta_x, delta_x, rho_x = params
+
+    # presume that the molecules are degraded in the same strain as they are produced
+    N_Y = N_X
+
+
+    dx_dt = N_X * gamma_x * (y ** n_y)/(1 + (theta_x*y)**n_y ) - N_Y * (delta_x * x) - rho_x * x
+    
+    return dx_dt
+
+# L_A ... intermediate
+# a ... out
+# b ... in
+# N_A ... number of cells
+def not_cell_wrapper(state, params):
+    L_A, a, b, N_A = state
+    
+    state_A = L_A, a, b, N_A, N_A
+    params_A = params
+
+    return not_cell(state_A, params_A)
+
+# a ... out
+# b ... in
+# N_A ... number of cells
+def yes_cell_wrapper(state, params):
+    a, b, N_A = state
+
+    state_A = a, b, N_A, N_A
+    params_A = params
+
+    return yes_cell(state_A, params_A)
+
+def not_model(state, T, params):
+    L_A, a, b, N_A = state
+
+    delta_L, gamma_L_A, n_b, theta_L_A, eta_a, omega_a, m_a, delta_a, delta_b, rho_a, rho_b, r_A = params
+
+    state_not = L_A, a, b, N_A
+    params_not = delta_L, gamma_L_A, n_b, theta_L_A, eta_a, omega_a, m_a, delta_a, rho_a
+    dL_A_dt, da_dt = not_cell_wrapper(state_not, params_not)
+    
+    db_dt = 0#- N_A * delta_b * b - rho_b * b
+
+    dN_A_dt = population(N_A, r_A)
+
+    return np.array([dL_A_dt, da_dt, db_dt, dN_A_dt])
+
+def yes_model(state, T, params):
+    a, b, N_A = state
+    
+    gamma_a, n_b, theta_a, delta_a, delta_b, rho_a, rho_b, r_A = params
+
+    state_yes = a, b, N_A
+    params_yes = gamma_a, n_b, theta_a, delta_a, rho_a
+    da_dt = yes_cell_wrapper(state_yes, params_yes)
+    
+    db_dt = 0 #- N_A * delta_b * b - rho_b * b
+
+    dN_A_dt = population(N_A, r_A)
+
+    return np.array([da_dt, db_dt, dN_A_dt])
+
+
+"""
+wrappers for scipy.integrate.ode
+"""
+
+def toggle_model_ODE(T, state, params):
+    return toggle_model(state, T, params)
+
+
+def not_model_ODE(T, state, params):
+    return not_model(state, T, params)
+    
+def yes_model_ODE(T, state, params):
+    return yes_model(state, T, params)
+
+def MUX_4_1_model_ODE(T, state, params):
+    return MUX_4_1_model(state, T, params)
+
+def CLB_model_ODE(T, state, params):
+    return CLB_model(state, T, params)
+
---
+++ ODEs/models_evol.py
@@ -0,0 +1,138 @@
+#
+# models of logic functions
+#
+
+from parameters import *
+import numpy as np
+from itertools import combinations
+
+#alpha = 10
+#Kd = 1
+#n = 2
+#delta = 1
+
+def YES(x, params = (alpha, Kd, n)):    
+    alpha, Kd, n = params
+
+    frac = (x/Kd)**n
+    dy_dt = alpha * (frac)/(1 + frac)
+    
+    return dy_dt
+
+def NOT(x, params = (alpha, Kd, n)):    
+    alpha, Kd, n = params
+
+    frac = (x/Kd)**n
+    dy_dt = alpha * (1)/(1 + frac)
+    
+    return dy_dt
+
+def AND(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (frac1*frac2)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# AND gate with an arbitrary number of inputs
+def generalised_AND(code, *ins, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    fracs = (np.array(ins)/Kd)**n
+        
+    dy_dt = 1
+    for frac, weight in zip(fracs, code):        
+        if weight == "1":            
+            dy_dt *= frac
+    
+    dy_dt *= alpha
+
+    denom = 0
+    for i in range(len(fracs)+1):
+        elts = list(combinations(fracs,i))
+        for elt in elts:            
+            denom += np.prod(elt)
+
+    dy_dt /= denom
+
+    return dy_dt
+
+# minterm 0
+def AND00(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (1)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# minterm 1
+def AND01(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (frac2)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# minterm 2
+def AND10(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (frac1)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# minterm 3
+def AND11(in1, in2, params = (alpha, Kd, n)):        
+    
+    return AND(in1, in2, params)
+
+# OR
+def OR(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (frac1 + frac2 + frac1*frac2)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# NOR
+def NOR(in1, in2, params = (alpha, Kd, n)):        
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    dy_dt = alpha * (1)/(1 + frac1 + frac2 + frac1*frac2)
+    
+    return dy_dt
+
+# XNOR / EQU
+def EQU(in1, in2, params = (alpha, Kd, n)):
+    alpha, Kd, n = params
+
+    frac1 = (in1/Kd)**n
+    frac2 = (in2/Kd)**n
+    
+    gate1 = (frac1*frac2)/(1 + frac1 + frac2 + frac1*frac2) # and
+    gate2 = (1)/(1 + frac1 + frac2 + frac1*frac2) # nor
+    dy_dt = alpha * (gate1 + gate2)
+
+    #x1 = AND(in1, in2, params=params)
+    #x2 = NOR(in1, in2, params=params)
+    #dy_dt=OR(x1, x2, params=params) # wrong - uses alpha**2 instead of alpha
+
+    return dy_dt
+
+# degradation
+def degrade(x, delta=delta):
+    dx_dt = - x * delta
+    
+    return dx_dt
